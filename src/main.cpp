9447ad7ae67d232126af89f7f8eedbe7564065a8
Chris <osh1996@gmail.com>
2018-11-16 13:41:57 -0500

    needs debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <TimerOne.h> // make sure to install this library to run!!
+#include <stdio.h>
 
 int dataPin = 3; //input from sensor
 int pinA = 4;
@@ -10,22 +11,35 @@ volatile int timer = 0;//timer in milliseconds
 volatile int buff[7]; //buffer to hold 8 bits of data
 volatile bool flag = 0;
 volatile int x = 0;
+char bin[7];
+
+
+void parseBuffer();
+void binaryToDecimal(char *binary, int length);
 
 void isr() { //interrupt service routine triggered on Falling edge of signal
 noInterrupts();
 if(timer <= 16 || timer >= 12){ //start bit
   flag = 1; //start filling the buffer
 }
+else{
 if(flag) {
   buff[x] = timer;
 }
 if(x == 7) {
   x = 0;
+  Serial.println("got here!");
+  parseBuffer();
   flag = 0;//reset all values when buffer is full
   timer = 0;
 }
+}
 //Serial.println(timer);
 interrupts();
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinCprime, LOW);
+digitalWrite(pinC, LOW);
 }
 
 void callback(){
@@ -50,9 +64,50 @@ void loop() {
 }
 
 void parseBuffer(){
-
+noInterrupts();
+for(x;x<7;x++){
+  if(buff[x] <= 1){
+    bin[x] = 0;
+  }
+  else{
+    bin[x] = 1;
+  }
+}
+binaryToDecimal(bin, 8);
+interrupts();
 }
 
-void binaryToLED(){
-
+void binaryToDecimal(char *binary, int length) {
+    int i;
+    unsigned long decimal = 0;
+    unsigned long weight = 1;
+    binary += length - 1;
+    weight = 1;
+    for(i = 0; i < length; ++i, --binary)
+    {
+        if(*binary == "1")
+            decimal += weight;
+        weight *= 2;
+    }
+    //char led;
+    if (decimal == 162){
+      //led = "A";
+      Serial.println("A");
+      digitalWrite(pinA, HIGH);
+    }
+    else if(decimal == 42){
+      //led = "B";
+            Serial.println("B");
+      digitalWrite(pinB, HIGH);
+    }
+    else if(decimal == 52){
+      //led = "Cprime";
+      Serial.println("Cprime");
+      digitalWrite(pinCprime, HIGH);
+    }
+    else{
+      //led = "C";
+      Serial.println("C");
+      digitalWrite(pinC, HIGH);
+    }
 }
